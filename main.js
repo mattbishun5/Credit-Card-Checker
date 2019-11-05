8512801840bed8d99dfb1c85498efa1e44c9d00e
Matt <32273032+mattbishun5@users.noreply.github.com>
2019-11-05 18:39:18 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Arrays belong to Codecademy
 // All valid credit card numbers
 const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
 const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
@@ -23,7 +24,7 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
 
 
-// Add your functions below:
+// My functions below:
 const validateCred = (card) => {
   let cardInverted = card.slice().reverse(); //reversing Credit card number and then iterate to the left. As per lohn's algorithm.
   
